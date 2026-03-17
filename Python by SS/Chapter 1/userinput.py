3042e76262e721fdc269b827e0dd69fb96625dd8
Himanshu Pandey <himanshupandey2899@gmail.com>
2026-03-18 00:51:41 +0530

    Progress Update

--- Python by SS/Chapter 1/userinput.py
+++ Python by SS/Chapter 1/userinput.py
@@ -4,8 +4,12 @@ print("A sample program to understand user input method in Python")  # Printing 
 
 name = input("Enter your good name: ")  # Taking input from the user and storing it in a variable called 'name'
 
-print("Hello, " , name , "!")  # Printing a greeting message with the user's name
+age = input("Enter your age: ")  # Taking input from the user and storing it in a variable called 'age'
 
-print("Hello, " , name + "!")  # Printing a greeting message with the user's name
+print("Hello, " , name , "!" , "\n" , "Your age is " , age)  # Printing a greeting message with the user's name and age
 
-print("Hello, " + name , "!")  # Printing a greeting message with the user's name
+print("Hello, " + name , "!" + "\n" , "Your age is " + age)  # Printing a greeting message with the user's name and age
+
+print("Hello, " + name , "!" , "\n" , "Your age is " , age)  # Printing a greeting message with the user's name and age
+
+print("Hello, " + name + "!" + "\n" + "Your age is " + age)  # Printing a greeting message with the user's name and age
