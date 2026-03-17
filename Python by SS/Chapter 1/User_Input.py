fb401dbf1a5556ae6d6678907197dda5c00758f9
Himanshu Pandey <himanshupandey2899@gmail.com>
2026-03-18 00:27:55 +0530

    Progress Update

---
+++ Python by SS/Chapter 1/User_Input.py
@@ -0,0 +1,7 @@
+# Program to take input from the user and print it
+
+print("Enter your name:")
+
+name = input()  # Taking input from the user and storing it in a variable called 'name'
+
+print("Hello, " + name + "!")  # Printing a greeting message with the user's name
---
+++ Python by SS/Chapter 1/userinput.py
@@ -0,0 +1,11 @@
+# Program to take input from the user and print it
+
+print("A sample program to understand user input method in Python")  # Printing a message to the user
+
+name = input("Enter your good name: ")  # Taking input from the user and storing it in a variable called 'name'
+
+print("Hello, " , name , "!")  # Printing a greeting message with the user's name
+
+print("Hello, " , name + "!")  # Printing a greeting message with the user's name
+
+print("Hello, " + name , "!")  # Printing a greeting message with the user's name
