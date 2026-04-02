38303552fc6b8477a93bc7355cfea3cf4b78a0a7
Himanshu Pandey <himanshupandey2899@gmail.com>
2026-04-02 20:03:39 +0530

    Progress Update

---
+++ Python by SS/Chapter 1/Assignment.py
@@ -0,0 +1,7 @@
+# Question - Take diameter as input and calculate the area of a circle.
+
+# Area of a circle = 2*3.14*r
+
+d = input("Diameter of the circle: ") # To take diameter of a circle
+
+print("Area of a circle = " + 3.14*d)
